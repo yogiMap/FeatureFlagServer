45a2392a3894191cb01b7ff690f722e97debf571
gayfulina <irinmdr@gmail.com>
2021-04-19 14:10:28 -0700

    flag-group4

--- src/modules/flag/controllers/create.js
+++ src/modules/flag/controllers/create.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
-//import Flag from '../Model';
 import message from '../../utils/messages';
 import analytics from '../../analytics/controllers/analytics';
 import { get } from 'lodash';
 import createFlagQuery from '../queries/create';
 import addFlagToGroupQuery from '../queries/addFlagToGroup';
+import Flag from '../Model';
+import escapeRegExp from '../../utils/escapeRegExp';
 
 export default async function flagCreate(req, res) {
   // Создаем id материала который будет создан
@@ -17,25 +18,31 @@ export default async function flagCreate(req, res) {
   const name = get(req, 'body.name');
   const value = get(req, 'body.value');
   const description = get(req, 'body.description');
-  const group = get(req, 'body.group');
+  const groupId = get(req, 'body.groupId');
+
+  const isFlagExist = await Flag.count({ name: { $eq: escapeRegExp(name) } });
+
+  if (isFlagExist) {
+    return res.status(400).json(message.fail('Flag exist'));
+  }
 
   const createFlagQueryResult = await createFlagQuery({
     _id,
     name,
     value,
     description,
-    group,
+    group: groupId,
     owner: userId,
   });
 
   const addFlagToGroupResult = await addFlagToGroupQuery({
     flagId: _id,
-    groupId: group,
+    groupId,
   });
 
-  console.log(addFlagToGroupResult);
+  console.log(addFlagToGroupResult, createFlagQueryResult);
 
-  if (createFlagQueryResult.success) {
+  if (createFlagQueryResult.success && addFlagToGroupResult.success) {
     res.status(200).json(createFlagQueryResult);
   } else {
     const analyticsId = analytics('FLAG_CREATE_ERROR', {
